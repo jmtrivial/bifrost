21e40d10d1117ef0dcadec01ec47c6d997ceec68
Jean-Marie Favreau <J-Marie.Favreau@udamail.fr>
2015-04-14 11:30:34 +0200

    Add a speech engine (with replay functionality)

---
+++ bifrost/src/main/java/info/jmfavreau/bifrost/speech/SpeechEngine.java
@@ -0,0 +1,66 @@
+/*
+ * Bifröst - A camera-to-speech system for Android
+ * Copyright (C) 2015 Jean-Marie Favreau <jeanmarie.favreau@free.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
+
+package info.jmfavreau.bifrost.speech;
+
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+
+import java.util.Locale;
+
+/**
+ * Created by Jean-Marie Favreau on 14/04/15.
+ */
+public class SpeechEngine {
+    String previousMessage = null;
+    long previousTime;
+
+    TextToSpeech ttobj = null;
+
+    SpeechEngine(Context appContext) {
+        ttobj = new TextToSpeech(appContext,
+                new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        if(status != TextToSpeech.ERROR){
+                            ttobj.setLanguage(Locale.getDefault());
+                        }
+                    }
+                });
+    }
+
+    void replay() {
+        if (previousMessage != null) {
+            long now =  System.currentTimeMillis();
+            if (now - previousTime > 300000) { // 5 minutes after, it's too late
+                previousMessage = null;
+            }
+            else {
+                speak(previousMessage);
+            }
+        }
+    }
+
+    void speak(String message) {
+        previousMessage = message;
+        previousTime = System.currentTimeMillis();
+        if (ttobj != null)
+            ttobj.speak(previousMessage, TextToSpeech.QUEUE_FLUSH, null, null);
+    }
+}
